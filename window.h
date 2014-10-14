c411d8a5ee4f940e8332cd1ed2f6944f3943c094
Andy Herbert <andy.herbert@gmail.com>
2014-10-14 22:42:32 +0100

    Remove references to old files.

--- window.h
+++
@@ -1,9 +0,0 @@
-#ifndef WINDOW_H_A2935005
-#define WINDOW_H_A2935005
-
-#include <stdbool.h>
-#include "canvas.h"
-
-bool show_window(Canvas *canvas, bool full_screen);
-
-#endif /* end of include guard: WINDOW_H_A2935005 */
